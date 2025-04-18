49ca6859a5d317eaf0b6afe7d8294b0fabe2da6b
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-04-18 10:59:54 -0300

    questao 02

---
+++ questao02/script.js
@@ -0,0 +1,9 @@
+let prioridade = 3
+
+if (prioridade == 1) {
+    console.log('Prioridade baixa!')
+} else if (prioridade == 2) {
+    console.log('Prioridade média!')
+} else {
+    console.log('Prioridade alta!')
+}
