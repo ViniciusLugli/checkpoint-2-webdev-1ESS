d1b3f1d71b6f9db6e206e5984021fa4c9eef64f4
matheusvictorio <matheusvic2016@gmail.com>
2025-04-19 12:52:28 -0300

    Questão 4

--- questoes/script.js
+++ questoes/script.js
@@ -1,63 +1,68 @@
 // Questão 01
 
-console.log('== Questão 01 ==')
+console.log("== Questão 01 ==");
 
-let tarefaConcluida = true
+let tarefaConcluida = true;
 
 if (tarefaConcluida == true) {
-    console.log('Tarefa Concluída!')
+  console.log("Tarefa Concluída!");
 } else {
-    console.log('Tarefa não Concluída!')
+  console.log("Tarefa não Concluída!");
 }
 
 // Questão 02
 
-console.log('== Questão 02 ==')
+console.log("== Questão 02 ==");
 
-let prioridade = 3
+let prioridade = 3;
 
 if (prioridade == 1) {
-    console.log('Prioridade baixa!')
+  console.log("Prioridade baixa!");
 } else if (prioridade == 2) {
-    console.log('Prioridade média!')
+  console.log("Prioridade média!");
 } else {
-    console.log('Prioridade alta!')
+  console.log("Prioridade alta!");
 }
 
 // Questão 03
 
-console.log('== Questão 03 ==')
+console.log("== Questão 03 ==");
 
-let diaSemana = new Date().getDay()
+let diaSemana = new Date().getDay();
 // Datas teste: '2025-04-6' - '2025-04-7' - '2025-04-8' - '2025-04-9' - '2025-04-10' - '2025-04-11' - '2025-04-12'
 
-console.log(`O número da semana é ${diaSemana}, então:`)
+console.log(`O número da semana é ${diaSemana}, então:`);
 
 switch (diaSemana) {
-    case 0:
-        console.log('Domingo')
-        break
-    case 1:
-        console.log('Segunda')
-        break
-    case 2:
-        console.log('Terça')
-        break
-    case 3:
-        console.log('Quarta')
-        break
-    case 4:
-        console.log('Quinta')
-        break
-    case 5:
-        console.log('Sexta')
-        break
-    case 6:
-        console.log('Sábado')
-        break
-    default:
-        console.log('Dia Inválido')
-        break
+  case 0:
+    console.log("Domingo");
+    break;
+  case 1:
+    console.log("Segunda");
+    break;
+  case 2:
+    console.log("Terça");
+    break;
+  case 3:
+    console.log("Quarta");
+    break;
+  case 4:
+    console.log("Quinta");
+    break;
+  case 5:
+    console.log("Sexta");
+    break;
+  case 6:
+    console.log("Sábado");
+    break;
+  default:
+    console.log("Dia Inválido");
+    break;
 }
 
 // Questão 04
+console.log("== Questão 04 ==");
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
