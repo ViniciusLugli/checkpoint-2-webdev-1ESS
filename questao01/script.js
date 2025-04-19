dd299dbd113cc5cf162c6569bd8442937c34c1ba
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-04-19 11:47:05 -0300

    Ajuste das Questoes

--- questao01/script.js
+++
@@ -1,7 +0,0 @@
-let tarefaConcluida = true
-
-if (tarefaConcluida == true) {
-    console.log('Tarefa Concluída!')
-} else {
-    console.log('Tarefa não Concluída!')
-}
--- questoes/script.js
+++ questoes/script.js
@@ -1,3 +1,33 @@
+// Questão 01
+
+console.log('== Questão 01 ==')
+
+let tarefaConcluida = true
+
+if (tarefaConcluida == true) {
+    console.log('Tarefa Concluída!')
+} else {
+    console.log('Tarefa não Concluída!')
+}
+
+// Questão 02
+
+console.log('== Questão 02 ==')
+
+let prioridade = 3
+
+if (prioridade == 1) {
+    console.log('Prioridade baixa!')
+} else if (prioridade == 2) {
+    console.log('Prioridade média!')
+} else {
+    console.log('Prioridade alta!')
+}
+
+// Questão 03
+
+console.log('== Questão 03 ==')
+
 let diaSemana = new Date().getDay()
 // Datas teste: '2025-04-6' - '2025-04-7' - '2025-04-8' - '2025-04-9' - '2025-04-10' - '2025-04-11' - '2025-04-12'
 
@@ -29,3 +59,5 @@ switch (diaSemana) {
         console.log('Dia Inválido')
         break
 }
+
+// Questão 04
